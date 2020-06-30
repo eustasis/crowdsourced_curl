40524d6426982cb836731e166bd06526ee505aed
eustasis <63314783+eustasis@users.noreply.github.com>
2020-06-30 13:52:36 +0100

    Update streamlit_final_git.py

--- streamlit_final_git.py
+++ streamlit_final_git.py
@@ -5,17 +5,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import linear_kernel
 from PIL import Image
-
-path = (r'C:\Users\mupsi\Desktop\crowdsourced_curl\full_dataset_topics_24jun2.csv')
+~
+path = (r'~final_dataset_for_streamlit.csv')
 def load_csv(path):
     df1 = pd.read_csv(path, dtype='string')
-    df2 = df1[['Link', 'Text', 'Hairtype','lemmatized_txt',  'First_Topic', 'Second_Topic', 'Third_Topic', 'Fourth_Topic']]
+    df2 = df1[['Link', 'Raw_Text', 'Hairtype','First_Topic_Name', 'Second_Topic_Name', 'Third_Topic_Name', 'Fourth_Topic_Name']]
     return df2
 
 
-cust_stopwords = text.ENGLISH_STOP_WORDS.union(['2a', '2b', '2c', '3a', '3b', '3c', '4a', '4b', '4c', 'just', 've', 'wa', 'don', 'using', 'really', 'routine', 'know',
-                                         'ha', 'cg', 'amp', 'work', 'try', 'used', 'x200b', 'notext', 'make', 'low', 'year', 'love', 'think', 'help', 'type', 'need', 'cut', 'lot', 'week', 'maybe', 'sure'])
-tfidf = TfidfVectorizer(stop_words=cust_stopwords) #no n-gram here, go back and fix this BEFORE THE LEMMATIZATION
+tfidf = TfidfVectorizer(stop_words=cust_stopwords) 
 
 stream_dataset = load_csv(path)
 
@@ -27,9 +25,9 @@ def curl_query(product, stream_dataset, tfidf):
     Return most similar based on cosine similarity
     
     """
-    df_dum = pd.DataFrame([product], columns=['lemmatized_txt'])
+    df_dum = pd.DataFrame([product], columns=['Raw_Text'])
     df_dum = pd.concat([stream_dataset, df_dum])
-    query_vectors = tfidf.fit_transform(df_dum['lemmatized_txt'])
+    query_vectors = tfidf.fit_transform(df_dum['Raw_Text'])
     cosine_similarities=linear_kernel(query_vectors[-1], query_vectors).flatten()
     related_docs = np.where((cosine_similarities>0.3) & (cosine_similarities<1))
     
@@ -41,7 +39,7 @@ def curl_query(product, stream_dataset, tfidf):
         st.write("No results")
     else:
         top_output = df_dum.loc[top_docs]
-        output = top_output[['Hairtype', 'Text','First_Topic', 'Second_Topic', 'Third_Topic', 'Fourth_Topic']]
+        output = top_output[['Hairtype', 'Raw_Text','First_Topic_Name', 'Second_Topic_Name', 'Third_Topic_Name', 'Fourth_Topic_Name']]
         output = output.sort_values(by=['Hairtype'])
         st.write("Posts are sorted by hair type. Hover your mouse over the post to read the whole post. Scroll to see the four main topics covered in the post.")
         st.write(output)
@@ -52,43 +50,20 @@ searchtext = st.text_input("What product would you like to learn about?")
 if searchtext:
     search_results = curl_query(searchtext, stream_dataset, tfidf)
     
-    image = Image.open(r'C:\Users\mupsi\Desktop\crowdsourced_curl\hair_types.png')
+    image = Image.open(r'~\hair_types.png')
     st.sidebar.subheader("Don't know your hair type? Check here: ")
     st.sidebar.image(image, width=450)
 
-    topics = stream_dataset['Third_Topic'].unique()
+    topics = stream_dataset['Third_Topic_Name'].unique()
     topic_choice = st.multiselect("Filter by topic: ", topics)
 
                
     if st.button("Submit topic filter"):
-            search_results= search_results.loc[search_results['First_Topic'].isin(topic_choice)\
-            | search_results['Second_Topic'].isin(topic_choice) |search_results['Third_Topic'].isin(topic_choice)\
-                                                      | search_results['Fourth_Topic'].isin(topic_choice)]
+            search_results= search_results.loc[search_results['First_Topic_Name'].isin(topic_choice)\
+            | search_results['Second_Topic_Name'].isin(topic_choice) |search_results['Third_Topic_Name'].isin(topic_choice)\
+                                                      | search_results['Fourth_Topic_Name'].isin(topic_choice)]
             st.write(search_results)
     else:
         st.write(" ")
 else:
     st.write(" ")
-
-
- ########## to filter by hairtype: not necessary given hairtype column: ##########   
-    
-#    image = Image.open(r'C:\Users\mupsi\Desktop\crowdsourced_curl\hair_types.png')
-#    st.subheader("Check your hair type and filter your results: ")
-#    st.image(image, width=450)#
-
-#    hairtypes = search_results['Hairtype'].unique()
-#    hairtype_choice = st.multiselect('',hairtypes)
-    
-#    if st.button("Submit hair type"):
-#        search_results_hair = search_results[search_results['Hairtype'].isin(hairtype_choice)]
- #       st.write(search_results_hair)
-        
-#        topics = stream_dataset['Third_Topic'].unique()
-#        topic_choice = st.multiselect("Filter by topic: ", topics)
-#                
-#        if st.button("Submit topic filter"):
-#            search_results_hair_topic = search_results_hair.loc[search_results['First_Topic'].isin(topic_choice)| search_results_hair['Second_Topic'].isin(topic_choice) |search_results_hair['Third_Topic'].isin(topic_choice)| search_results_hair['Fourth_Topic'].isin(topic_choice)]
-#            st.write(search_results_hair_topic, astype('object'))
-#else:
-#    st.write('')
